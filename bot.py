869bf8da4f87f060f7d44df9241bc26c686bd11c
ninotg <167024122+ninotg@users.noreply.github.com>
2025-01-28 18:51:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
-#(©)Codexbotz
+# fix for current pyrogram 
+from pyrogram import utils
+
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+utils.get_peer_type = get_peer_type_new#(©)Codexbotz
 
 from aiohttp import web
 from plugins import web_server
